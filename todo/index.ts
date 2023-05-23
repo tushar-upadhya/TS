db9fa2eb01d5096bf880a0e6d8ba56c97b94a1b1
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-05-23 16:44:54 +0530

    add feature

--- todo/index.ts
+++ todo/index.ts
@@ -1,1 +1,22 @@
-console.log("a");
+// console.log("a");
+import { v4 as uuidV4 } from "uuid";
+
+const list = document.querySelector<HTMLLIElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+// const form = document.querySelector<HTMLFormElement>("#new-task");
+
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (input?.value == "" || input?.value == null) return;
+
+    const task = {
+        id: "",
+        title: "",
+        completed: false,
+        createdAt: new Date(),
+    };
+    input.value;
+});
