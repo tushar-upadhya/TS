721f7b16ffbae623f532fd8363f2281f8649edbe
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-05-23 16:53:16 +0530

    bug

--- todo/index.ts
+++ todo/index.ts
@@ -1,6 +1,13 @@
 // console.log("a");
 import { v4 as uuidV4 } from "uuid";
 
+interface ITask {
+    id: string;
+    title: string;
+    completed: boolean;
+    createdAt: Date;
+}
+
 const list = document.querySelector<HTMLLIElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
@@ -12,11 +19,26 @@ form?.addEventListener("submit", (e) => {
 
     if (input?.value == "" || input?.value == null) return;
 
-    const task = {
-        id: "",
-        title: "",
+    const newTask: ITask = {
+        id: uuidV4(),
+        title: input.value,
         completed: false,
         createdAt: new Date(),
     };
-    input.value;
+
+    addlistItem(newTask);
 });
+
+function addlistItem(task: ITask) {
+    const item = document.createElement("li");
+
+    const label = document.createElement("label");
+
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+
+    label.append(checkBox, task.title);
+
+    item.append(label);
+    list?.append(item);
+}
